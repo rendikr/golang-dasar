bc1bc0f638bdf1e72649939d928588baa6c1a898
Rendi K <rendi.krinaldi@gmail.com>
2023-11-23 18:33:21 +0700

    konversi tipe data

---
+++ 1-golang-dasar/conversion.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nilai32 int32 = 32768
+	var nilai64 = int64(nilai32)
+	var nilai16 = int16(nilai32)
+
+	fmt.Println(nilai64)
+	fmt.Println(nilai32)
+	fmt.Println(nilai16)
+
+	var name = "John Doe"
+	var firstChar = name[0]
+	var firstCharString = string(firstChar)
+
+	fmt.Println(name)
+	fmt.Println(firstChar)
+	fmt.Println(firstCharString)
+}
