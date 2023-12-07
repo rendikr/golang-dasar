0c5909a99c6b97a5ef6f4fb2f0c8bab6f1d699b5
Rendi K <rendi.krinaldi@gmail.com>
2023-12-07 11:16:44 +0700

    pointer

---
+++ 1-golang-dasar/pointer.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type Address struct {
+	City, Province, Country string
+}
+
+func main() {
+	// pass by value
+	address1 := Address{"Subang", "Jawa Barat", "Indonesia"}
+	address2 := address1 // copy value
+
+	address2.City = "Bandung"
+	fmt.Println(address1) // tidak berubah
+	fmt.Println(address2) // city berubah menjadi Bandung
+
+	// pass by reference
+	address3 := Address{"Subang", "Jawa Barata", "Indonesia"}
+	address4 := &address3 // reference
+
+	address4.City = "Bandung"
+	fmt.Println(address3) // city ikut berubah
+	fmt.Println(address4) // city berubah menjadi Bandung
+}
