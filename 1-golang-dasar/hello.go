25a057f96fd0c39c3b3ee55e187b26c17844af47
Rendi K <rendi.krinaldi@gmail.com>
2023-12-07 12:16:16 +0700

    access modifier

--- 1-golang-dasar/hello.go
+++ 1-golang-dasar/hello.go
@@ -8,4 +8,8 @@ import (
 func main() {
 	result := helper.SayHello("John")
 	fmt.Println(result)
+
+	fmt.Println(helper.Application)
+	// fmt.Println(helper.version)            // returns undefined
+	// fmt.Println(helper.sayGoodbye("John")) // returns undefined
 }
--- 1-golang-dasar/helper/helper.go
+++ 1-golang-dasar/helper/helper.go
@@ -1,5 +1,12 @@
 package helper
 
+var version = "1.0.0"      // non accessible to other package
+var Application = "golang" // accessible to other package
+
+func sayGoodbye(name string) string {
+	return "Goodbye, " + name
+}
+
 func SayHello(name string) string {
 	return "Hello " + name
 }
