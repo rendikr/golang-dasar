03096a3920cfe7909745689ea3be46ea984d1851
Rendi K <rendi.krinaldi@gmail.com>
2023-12-20 20:30:34 +0700

    failing a test

--- 4-golang-unit-test/helper/hello_world_test.go
+++ 4-golang-unit-test/helper/hello_world_test.go
@@ -2,10 +2,18 @@ package helper
 
 import "testing"
 
-func TestHelloWorld(t *testing.T) {
+func TestHelloWorldJohn(t *testing.T) {
 	result := HelloWorld("John")
 
 	if result != "Hello John" {
-		panic("Result is not 'Hello John'")
+		t.Error("Result should be Hello John") // equal to t.Fail()
+	}
+}
+
+func TestHelloWorldJane(t *testing.T) {
+	result := HelloWorld("Jane")
+
+	if result != "Hello Jane" {
+		t.Fatal("Result should be Hello Jane") // equal to t.FailNow()
 	}
 }
