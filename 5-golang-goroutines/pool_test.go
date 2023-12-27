7aa9e6cf42eb065dad7b5c41f83c80ffeabb9ab8
Rendi K <rendi.krinaldi@gmail.com>
2023-12-27 11:56:04 +0700

    sync.Pool

---
+++ 5-golang-goroutines/pool_test.go
@@ -0,0 +1,32 @@
+package golang_goroutines
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestPool(t *testing.T) {
+	var pool = sync.Pool{
+		New: func() interface{} {
+			return "New"
+		},
+	}
+
+	pool.Put("John")
+	pool.Put("Jane")
+	pool.Put("Peter")
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			data := pool.Get()
+			fmt.Println(data)
+			time.Sleep(1 * time.Second)
+			pool.Put(data)
+		}()
+	}
+
+	time.Sleep(11 * time.Second)
+	fmt.Println("Finished!")
+}
