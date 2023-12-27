a205d341a6fd11bfcca5cfed3f9e387a061a6520
Rendi K <rendi.krinaldi@gmail.com>
2023-12-27 15:32:15 +0700

    context with value

--- 6-golang-context/context_test.go
+++ 6-golang-context/context_test.go
@@ -13,3 +13,27 @@ func TestContext(t *testing.T) {
 	todo := context.TODO()
 	fmt.Println(todo)
 }
+
+func TestContextWithValue(t *testing.T) {
+	contextA := context.Background()
+
+	// creating sub context based on a parent context, with key - value pair
+	contextB := context.WithValue(contextA, "b", "B")
+	contextC := context.WithValue(contextA, "c", "C")
+
+	contextD := context.WithValue(contextB, "d", "D")
+	contextE := context.WithValue(contextB, "e", "E")
+
+	contextF := context.WithValue(contextC, "f", "F")
+
+	fmt.Println(contextA)
+	fmt.Println(contextB)
+	fmt.Println(contextC)
+	fmt.Println(contextD)
+	fmt.Println(contextE)
+	fmt.Println(contextF)
+
+	fmt.Println(contextF.Value("f")) // returns value
+	fmt.Println(contextF.Value("c")) // returns value because context F (with value "f") is a child of context C (with value "c")
+	fmt.Println(contextF.Value("b")) // doesn't returns value because context F and any of it's parents, doesn't have any context that has "b" value
+}
