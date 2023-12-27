03c5bc405bf9239a7d6bafd0381daaab04e0a51a
Rendi K <rendi.krinaldi@gmail.com>
2023-12-27 16:04:54 +0700

    context with deadline

--- 6-golang-context/context_test.go
+++ 6-golang-context/context_test.go
@@ -54,6 +54,7 @@ func CreateCounter(ctx context.Context) chan int {
 			default:
 				destination <- counter
 				counter++
+				time.Sleep(1 * time.Second)
 			}
 		}
 	}()
@@ -88,7 +89,26 @@ func TestContextWithTimeout(t *testing.T) {
 	fmt.Println("Total Goroutine:", runtime.NumGoroutine())
 
 	parent := context.Background()
-	ctx, cancel := context.WithTimeout(parent, 5*time.Second) // set the process should not be exceeding the maximum time
+	ctx, cancel := context.WithTimeout(parent, 5*time.Second) // set the process should not be exceeding the maximum time, will trigger cancel() automatically after n time
+	defer cancel()
+
+	destination := CreateCounter(ctx)
+	fmt.Println("Total Goroutine:", runtime.NumGoroutine())
+
+	for n := range destination {
+		fmt.Println("Counter:", n)
+	}
+
+	time.Sleep(2 * time.Second)
+
+	fmt.Println("Total Goroutine:", runtime.NumGoroutine())
+}
+
+func TestContextWithDeadline(t *testing.T) {
+	fmt.Println("Total Goroutine:", runtime.NumGoroutine())
+
+	parent := context.Background()
+	ctx, cancel := context.WithDeadline(parent, time.Now().Add(5*time.Second)) // set the process should not be exceeding the maximum time, will trigger cancel() automatically at a specific time
 	defer cancel()
 
 	destination := CreateCounter(ctx)
