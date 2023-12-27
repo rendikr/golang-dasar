6f36f26ce6e6db772aea3750d00d263d843957cb
Rendi K <rendi.krinaldi@gmail.com>
2023-12-27 10:14:17 +0700

    channel as paramtere

--- 5-golang-goroutines/channel_test.go
+++ 5-golang-goroutines/channel_test.go
@@ -21,3 +21,20 @@ func TestCreateChannel(t *testing.T) {
 	data := <-channel // receiving new channel data to a newly var data
 	fmt.Println("data:", data)
 }
+
+func GiveMeResponse(channel chan string) { // channel will be "pass by reference" as a default, so we don't need to use pointer on using channel as parameter
+	time.Sleep(2 * time.Second)
+	channel <- "Jane Doe"
+}
+
+func TestChannelAsParameter(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go GiveMeResponse(channel)
+
+	data := <-channel
+	fmt.Println(data)
+
+	time.Sleep(5 * time.Second)
+}
