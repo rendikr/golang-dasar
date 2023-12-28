bcbdce3028f1f700480922ccf6a7a3ec16be2315
Rendi K <rendi.krinaldi@gmail.com>
2023-12-28 15:17:27 +0700

    open database connection

--- 7-golang-database/database_test.go
+++ 7-golang-database/database_test.go
@@ -1,6 +1,7 @@
 package golang_database
 
 import (
+	"database/sql"
 	"testing"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -8,4 +9,12 @@ import (
 
 func TestEmpty(t *testing.T) {
 
+}
+
+func TestOpenConnection(t *testing.T) {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/golang_dasar")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 }
