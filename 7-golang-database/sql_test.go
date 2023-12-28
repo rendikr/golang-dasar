892f90e0a1dbb2cce6a1b67ec8bc46acf2dc8774
Rendi K <rendi.krinaldi@gmail.com>
2023-12-28 15:40:05 +0700

    execute sql query

---
+++ 7-golang-database/sql_test.go
@@ -0,0 +1,22 @@
+package golang_database
+
+import (
+	"context"
+	"fmt"
+	"testing"
+)
+
+func TestExecSql(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	query := "INSERT INTO customer(id, name) VALUES('john', 'John')"
+	_, err := db.ExecContext(ctx, query)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Success Insert New Customer")
+}
