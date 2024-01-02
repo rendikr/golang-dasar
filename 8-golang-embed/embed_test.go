12bbdf026ceb92c8b6491a87c6b933b51de7eee1
Rendi K <rendi.krinaldi@gmail.com>
2024-01-02 13:50:46 +0700

    embed []byte to file

--- 8-golang-embed/embed_test.go
+++ 8-golang-embed/embed_test.go
@@ -3,12 +3,24 @@ package embed
 import (
 	_ "embed"
 	"fmt"
+	"io/fs"
+	"io/ioutil"
 	"testing"
 )
 
 //go:embed version.txt
 var version string
 
+//go:embed char.png
+var char []byte
+
 func TestString(t *testing.T) {
 	fmt.Println(version)
+}
+
+func TestByteArray(t *testing.T) {
+	err := ioutil.WriteFile("char_next.png", char, fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
 }
